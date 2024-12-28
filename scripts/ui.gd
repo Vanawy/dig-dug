20bb90e95199a18c5243c7be96d9259c522ebb31
Ilya Sakharchuk <saharchuk999@gmail.com>
2024-12-29 01:56:34 +0300

    simple level change text

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -5,6 +5,9 @@ class_name GameUI
 @export var level_label: Label
 @export var score_label: Label
 
+@export var level_change_label: Label
+@export var level_change_animation: AnimationPlayer
+
 var player_hp: int = 0:
 	set(v):
 		player_hp_progress.value = v
@@ -12,7 +15,13 @@ var player_hp: int = 0:
 var level: int = 0:
 	set(v):
 		level_label.text = str(v).pad_zeros(2)
+		level_change_animation.play("fadeout")
+		level_change_label.text = "Level " + level_label.text
 
 var score: int = 0:
 	set(v):
 		score_label.text = str(v).pad_zeros(7)
+
+
+func _ready() -> void:
+	level_change_label.visible = false
